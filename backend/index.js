15a3accafcc51d128f1841ef0456a5f7bf2b690f
Mohammed Sinan <sinanksinu0@gmail.com>
2025-03-09 11:26:37 -0700

    Deploy23

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-  origin: "*",
+  origin: "https://parttime-connect-pkkk.vercel.app",
   credentials: true,
 };
 
